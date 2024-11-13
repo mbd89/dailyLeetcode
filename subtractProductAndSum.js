b482fada5e537c5aa30c24afda3ee966ee708e09
Makha.D <87713231+mbd89@users.noreply.github.com>
2024-11-13 12:55:28 +0000

    Add files via upload

---
+++ subtractProductAndSum.js
@@ -0,0 +1,49 @@
+/* 
+1281. Subtract the Product and Sum of Digits of an Integer
+Solved
+Easy
+Topics
+Companies
+Hint
+Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+ 
+
+Example 1:
+
+Input: n = 234
+Output: 15 
+Explanation: 
+Product of digits = 2 * 3 * 4 = 24 
+Sum of digits = 2 + 3 + 4 = 9 
+Result = 24 - 9 = 15
+Example 2:
+
+Input: n = 4421
+Output: 21
+Explanation: 
+Product of digits = 4 * 4 * 2 * 1 = 32 
+Sum of digits = 4 + 4 + 2 + 1 = 11 
+Result = 32 - 11 = 21
+ 
+*/
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+/**
+ * @param {string} command
+ * @return {string}
+ */
+var subtractProductAndSum = function(num) {
+  num = num.toString()
+  let sum = 0, product = 1
+  for(let i in num){
+  sum+=Number(num[i])
+  product*=Number(num[i])
+  } return product - sum
+  };
+
+
+
+  
+
