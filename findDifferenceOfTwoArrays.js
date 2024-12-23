2fc228609754eb39a26ce86f4114e447ebf63d7f
Makha.D <87713231+mbd89@users.noreply.github.com>
2024-12-23 12:56:40 +0000

    Add files via upload

---
+++ findDifferenceOfTwoArrays.js
@@ -0,0 +1,51 @@
+/* 
+Given two 0-indexed integer arrays nums1 and nums2, return a list answer of size 2 where:
+
+answer[0] is a list of all distinct integers in nums1 which are not present in nums2.
+answer[1] is a list of all distinct integers in nums2 which are not present in nums1.
+Note that the integers in the lists may be returned in any order.
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,2,3], nums2 = [2,4,6]
+Output: [[1,3],[4,6]]
+Explanation:
+For nums1, nums1[1] = 2 is present at index 0 of nums2, whereas nums1[0] = 1 and nums1[2] = 3 are not present in nums2. Therefore, answer[0] = [1,3].
+For nums2, nums2[0] = 2 is present at index 1 of nums1, whereas nums2[1] = 4 and nums2[2] = 6 are not present in nums2. Therefore, answer[1] = [4,6].
+Example 2:
+
+Input: nums1 = [1,2,3,3], nums2 = [1,1,2,2]
+Output: [[3],[]]
+Explanation:
+For nums1, nums1[2] and nums1[3] are not present in nums2. Since nums1[2] == nums1[3], their value is only included once and answer[0] = [3].
+Every integer in nums2 is present in nums1. Therefore, answer[1] = [].
+
+*/
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[][]}
+ */
+var findDifference = function(nums1, nums2) {
+let arr = []
+let arr2 = []
+let arr3 = []
+for(let i = 0;i<nums1.length;i++){
+!nums2.includes(nums1[i])?arr.push(nums1[i]):null
+} 
+let newArr = [...new Set(arr)];
+
+for(let i = 0;i<nums2.length;i++){
+!nums1.includes(nums2[i])?arr2.push(nums2[i]):null
+} 
+let newArr2 = [...new Set(arr2)];
+
+arr3.push(newArr)
+arr3.push(newArr2)
+return arr3   
+};
+
+
